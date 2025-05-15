726860b5599792448bb6ff41b857e781ff14c46f
Hanny Paz <hwpazz@gmail.com>
2025-05-14 23:15:24 -0500

    se agrega la tarjeta para mostrar los producto y se añade el estilo css

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,28 @@
-export default function Card(imagen, title, descripcion, precio) {
+import { useState } from "react";
+import "../style/card.css";
+
+export default function Card({ imagen, titulo, descripcion, precio }) {
+  const [added, SetAdded] = useState(false);
+
   return (
     <>
-      <div className="tarjeta"></div>
+      <div className="tarjeta">
+        <img src={imagen} alt={titulo} className="tarjeta-imagen" />
+        <div className="tarjeta-contenido">
+          <h3 className="tarjeta-titulo">{titulo}</h3>
+          <p className="tarjeta-descripcion">{descripcion}</p>
+          <p className="tarjeta-precio">{precio}</p>
+        </div>
+        {added ? (
+          <button type="button" className="boton-quitar">
+            Quitar carrito
+          </button>
+        ) : (
+          <button type="button" className="boton-agregar">
+            Agregar Carrito
+          </button>
+        )}
+      </div>
     </>
   );
 }
--- src/pages/ComprasPage.jsx
+++ src/pages/ComprasPage.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
+import Card from "../components/Card";
 
 export const ComprasPage = () => {
   const [productos, setProductos] = useState([]);
 
   const fetchProductos = async () => {
-    const response = await fetch("https://fakestoreapi.in/api/products");
+    const response = await fetch("https://fakestoreapi.com/products");
     const data = await response.json();
     console.log(data);
     setProductos(data);
@@ -18,6 +19,16 @@ export const ComprasPage = () => {
     <>
       <h2>Compras: </h2>
       <hr />
+      {Array.isArray(productos) &&
+        productos.map((producto) => (
+          <Card
+            key={producto.id}
+            imagen={producto.image}
+            titulo={producto.title}
+            descripcion={producto.description}
+            precio={producto.price}
+          ></Card>
+        ))}
     </>
   );
 };
