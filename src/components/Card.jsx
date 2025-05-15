53d0654bfe4c197876039d72d37ad1ef7e08c1d4
Hanny Paz <hwpazz@gmail.com>
2025-05-14 21:16:33 -0500

    se agrego la API de productos por medio del fetch

---
+++ src/components/Card.jsx
@@ -0,0 +1,7 @@
+export default function Card(imagen, title, descripcion, precio) {
+  return (
+    <>
+      <div className="tarjeta"></div>
+    </>
+  );
+}
--- src/pages/ComprasPage.jsx
+++ src/pages/ComprasPage.jsx
@@ -4,12 +4,20 @@ export const ComprasPage = () => {
   const [productos, setProductos] = useState([]);
 
   const fetchProductos = async () => {
-    const response = await fetch();
+    const response = await fetch("https://fakestoreapi.in/api/products");
+    const data = await response.json();
+    console.log(data);
+    setProductos(data);
   };
 
   useEffect(() => {
     fetchProductos();
-  }, [third]);
+  }, []);
 
-  return <div>ComprasPage</div>;
+  return (
+    <>
+      <h2>Compras: </h2>
+      <hr />
+    </>
+  );
 };
